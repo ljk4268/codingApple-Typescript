cbf86a9ff1a1d0631df972dafe1889a166071bef
ljk4268 <ljk4268@kakao.com>
2022-09-09 21:03:13 +0900

    타입스크립트 기본 타입 정리 (primitive types)

---
+++ primitive.js
@@ -0,0 +1,19 @@
+let firstname = 'kim';
+let age = 32;
+let marrid = false;
+let member = ['kim', 'park'];
+let members = { member1: 'kim', member2: 'park' };
+let membersname = 'park';
+//문제
+// Q1. 여러분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다. 
+let myName = 'leejakyoung';
+let myage = 32;
+let placeofbirth = 'seoul';
+// Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
+let myfavoriteMusic = { title: '아무노래', artist: '지코' };
+// Q3. 다음과 같이 생긴 자료의 타입지정을 해보도록 합시다.
+let project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
---
+++ primitive.ts
@@ -0,0 +1,32 @@
+let firstname: string = 'kim';
+let age: number = 32;
+let marrid: boolean = false;
+
+let member: string[] = ['kim', 'park']
+let members: {member1:string, member2:string} = {member1:'kim', member2:'park' }
+
+
+let membersname = 'park'     
+
+//문제
+// Q1. 여러분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다. 
+
+let myName = 'leejakyoung';
+let myage = 32;
+let placeofbirth = 'seoul'
+
+// Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
+let myfavoriteMusic: {title: string; artist: string} = { title: '아무노래', artist: '지코'}
+
+// Q3. 다음과 같이 생긴 자료의 타입지정을 해보도록 합시다.
+
+let project : {
+  member : string[],
+  days : number,
+  started : boolean,
+}= {
+  member : ['kim', 'park'],
+  days : 30,
+  started : true,
+}
+
