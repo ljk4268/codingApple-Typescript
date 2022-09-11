a7baceaed24c97056e21d28c374b56b74ed7b9b5
ljk4268 <ljk4268@kakao.com>
2022-09-11 14:05:56 +0900

    22.09.11_함수에 타입 지정하는 법 & void 타입

--- index.ts
+++ index.ts
@@ -1,39 +1,84 @@
-// Union Type : 타입 2개 이상 합친 새로운 타입
-let 회원 : number | string = 'kim';
+// 함수에서만 쓸 수 있는 void타입
+// return 할 게 없는 경우
+// 실수로 return 하는걸 막아줄 수 있음. 
+function func(x:number) :void{
+  1 + 1
+}
+
+func(30)
+
+//파라미터가 옵션일 경우엔 파라미터변수?: 타입
+//함수 호출 후 파라미터 넣지 않아도 에러가 나지 않는다.
+// 변수?:number == 변수:number|undefined랑 같다.
+function test(x?:number){
+
+}
+test()
+
+//퀴즈: 이 함수는 왜 이럴까요?? 
+// 타입이 number로 확정지어져 있지 않기 때문에 타입스크립트에서 에러를 보낸다.
+
+function quiz(x:number|string){
+  // console.log(x + 3)
+}
+
+quiz(2)
+
+// 이름을 파라미터로 입력하면 콘솔창에 '안녕하세요 홍길동'을 출력 
+// 아무것도 파라미터로 입력하지 않고 함수를 사용하면 '이름이 없습니다.' 출력 
+
+function quiz1(x?:string) : string{
+  if ( typeof(x) == 'string'){
+    return '안녕하세요' + x
+  } else {
+    return '이름이 없습니다.'
+  }
+}
+
+console.log(quiz1())
 
 
-// 숫자 or 문자가 가능한 array / object 타입지정
-let 회원들 : (number|string)[] = [1,2,3];
-let 오브젝트 : { a : string | number }= { a : 123 }
+// 함수에 숫자 또는 문자를 집어넣으면 자릿수를 세어 출력해주는 함수
+function quiz2(x : string | number) : number{
+  if ( typeof(x) == "string" ){
+    return (x.length)
+  }
+  if ( typeof(x) == "number" ){
+    x = x.toString();
+    return (x.length)
+  }
+  
+  
+}
 
-// any 타입 : 모든 자료형 허용해줌
-// 그럼 타입스크립트 쓰는 이융가 없음. >> 타입실드 해제문법임.
-let 이름 : any;
-// 이름 = 123;
-// 이름 = true;
-이름 = [];
+console.log(quiz2(2456))
 
+// 결혼 가능 확률을 알려주는 함수 
+// 함수의 파라미터로 월소득(만원단위), 집보유여부(true/false), 매력점수 ('상' or '중' or '하') 를 입력받는다. 
+// 월소득은 만원 당 1점, 집보유시 500점 & 미보유시 0점, 매력점수는 '상'일 때만 100점으로 계산.
+// 총 점수가 600점 이상일 경우 '결혼가능'을 return 하기. con
 
-// unknown 타입 : any타입과 비슷하게 모든 자료형을 허용해줌
-// any보다 안전한 이유는 unknown일지라도 타입이 다른경우 연산을 해주지 않음. 
-// 정확한 타입끼리의 연산만 허락해줌. 
-let 이름임 : unknown;
-이름임 = 123;
-이름임 = {};
+function quiz3(월소득:number, 집보유여부:boolean, 매력:string): string {
+  let score = 0; 
+  let 월소득점수 = 월소득;
+  let 집보유여부점수 = 0;
+  let 매력점수 = 0;
+  
+  if ( 집보유여부 == true ){
+    집보유여부점수 = 500;
+  }
 
+  if ( 매력 == '상' ){
+    매력점수 = 100;
+  }
 
-// 아래와 같은 상황일 때 any는 버그를 잡아주지 않지만 
-// unknown은 버그를 잡아준다.
-// let 변수1 : string = 이름임;
+  score = 월소득점수 + 집보유여부점수 + 매력점수
 
-// 타입이 맞는데 연산이 안되는 경우 
-// string타입에 + 1은 허용 
-// number타입에 + 1은 허용 
-// string | number 타입에 + 1은 비허용 : string 또는 number 타입이기때문에 확정적으로 타입을 지정한게 아니니까! 
-let 나이 : string | number;
+  if ( score >= 600 ){
+    return '결혼가능'
+  } 
 
+  
+}
 
-let user : string = 'kim';
-let age1 : unknown = undefined;
-let marrid1 : boolean = false;
-let 철수 : unknown = [user, age1, marrid1]
+console.log(quiz3(100, false, '상'))
