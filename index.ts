850aa9a17c570e21a576b8c13f8ead2754c6feeb
ljk4268 <ljk4268@kakao.com>
2022-10-20 11:30:05 +0900

    Narrowing 할 수 있는 여러가지 방법

--- index.ts
+++ index.ts
@@ -1,68 +1,33 @@
-
-// 점 3개 붙이면 무제한 파라미터가 들어올 수 있다는 의미. 
-// 다른 파라미터가 있으면 맨 뒤에 사용해야 한다. 
-function 함수(...rest :number[]){
-  console.log(rest)
-
+// null & undefinde 타입체크하는 경우가 많다. 
+// && 연산자로 null & undefinde 타입체크하기 
+function 함수(a :string | undefined){
+  if ( a && typeof a === 'string' ){
+    // a가 undefined면 if문 실행이 안되고
+    // a가 string이면 if문이 실행된다.
+  }
 }
 
-함수(1,5,3,6,6)
 
 
-// destructurin 개념설명
-let {student, age} = {student : true, age : 20}
-let 오브젝트 = {student : true, age : 20}
+type Fish = { swim: string };
+type Bird = { fly: string };
+function 함수2(animal: Fish | Bird) {
+  if ('swim' in animal){
 
-
-
-function 함수1({student, age}){
-
-  console.log(student, age)
-
+  }
 }
 
-함수1({student : true, age : 20})
-
-// 숙제 1. 숫자 여러개를 입력하면 최댓값을 return 해주는 함수를 만들어봅시다. 
-
-function solution(...num :number[]){
-  let result = 0;
-
-  num.forEach((element)=>{
-
-    if (result < element){
-      result = element
-    }
-
-  })
-
-  console.log(result)
-
+type Car = {
+  wheel : '4개',
+  color : string
+}
+type Bike = {
+  wheel : '2개',
+  color : string
 }
 
-solution(6,3,7,2)
-
-// 숙제 2. 이렇게 생긴 object 자료를 파라미터로 입력할 수 있는 함수를 만들어봅시다. 
-
-type Obj = {
-  user :string,
-  comment :number[],
-  admin :boolean
+function 함수3(x : Car | Bike){
+  if ( x.wheel === '4개' ){
+    console.log('x는 Car타입이에요')
+  }
 }
-function solution2({user,comment,admin}:Obj){
-
-
-}
-
-solution2({ user : 'kim', comment : [3,5,4], admin : false })
-
-
-//숙제 3. 이렇게 생긴 array 자료를 파라미터로 입력할 수 있는 함수를 만들어봅시다. 
-type Arr = (number | string | boolean)[];
-function solution3([a,b,c]:Arr){
-
-
-}
-
-solution3([40, 'wine', false])
-
