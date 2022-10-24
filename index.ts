b0cca6cca27e16eb969fd0ce425a352c86ae32bb
ljk4268 <ljk4268@kakao.com>
2022-10-24 12:28:03 +0900

    타입을 파라미터로 입력하는 Generic

--- index.ts
+++ index.ts
@@ -1,43 +1,32 @@
-// private, protected는 class{}안에서만 사용가능
-// private를 쓰면 class 확장된 곳에서는 사용이 불가능! : 확장된 class 사용불가능!!
-// extends 된 곳에서도 사용하고 싶으면 protected : 확장된 class 사용가능!! 
-
-class User {
-  protected x = 10;
+interface LengthCheck {
+  length : number
 }
 
-class NewUser extends User {
-  doThis(){
-    this.x = 20;
-  }
+function 함수<MyType extends LengthCheck>(x: MyType){
+  return x.length
+}
+let a = 함수<string>('100')
+
+// 숙제1. 문자를 집어넣으면 문자의 갯수, array를 집어넣으면 array안의 자료 갯수를 콘솔창에 출력해주는 함수는 어떻게 만들까요? 
+
+function 함수2<Type extends string | string[]>(x :Type){
+  console.log(x.length);
 }
 
-let person = new NewUser();
+함수2<string>('hello')
+함수2<string[]>(['hello','kim'])
 
+// 숙제 2. Animal 이라는 타입이 있습니다.
 
-// static 키워드
-// static 키워드 붙이면 부모 class에만 직접 부여돼서 자식은 쓸 수 없다. 
-
-class User2 {
-
-  static skill = 'js';
-  intro = User2.skill + '전문가입니다';
-
+interface Animal {
+  name : string;
+  age : number 
 }
 
-let 철수 = new User2();
+let data = '{"name" : "dog", "age" : 1 }'
 
-
-//숙제 : User3.addOne() 쓸 때마다 x가 증가하는 함수
-
-class User3 {
-  private static x = 10;
-  public static y = 20;
-  
-  static addOne(num :number){
-    User3.x += num;
-  }
+function 함수3<Type>(x :string) :Type {
+  return JSON.parse(x);
 }
-
-
-
+let result = 함수3<Animal>(data)
+console.log(result)
