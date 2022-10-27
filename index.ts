7c7da25256187c8607f9c3e94e2ef35833b8d9d6
ljk4268 <ljk4268@kakao.com>
2022-10-27 10:32:37 +0900

    object 타입 변환기 만들기

--- index.ts
+++ index.ts
@@ -1,4 +1,49 @@
-let 이름 : string = '김';
-type Age = number
+// interface Person {
+//   age :number,
+//   name :string
+// }
 
-let 나이 :Age1 = 30;
+// type PersonKeys = keyof Person;
+// let a :PersonKeys = 'name'
+
+// let obj = {name : 'kim', age: 20}
+// Object.keys(obj)
+
+
+type Car = {
+  color :boolean,
+  model :boolean,
+  price :boolean | number
+}
+
+
+type TypeChanger<MyType> = {
+  [key in keyof MyType] :string
+}
+
+type 새로운타입 = TypeChanger<Car>
+
+
+// (숙제1) 다음 타입을 변환기를 돌려보십시오.
+
+type Bus = {
+  color : string,
+  model : boolean,
+  price : number
+}
+
+type TypeChanger2<MyType>{
+  [key in keyof MyType] :string|number
+}
+
+type newBusType = TypeChanger2<Bus>
+
+//(숙제2) 이런 변환기는 어떻게 만들어야할까요?
+//내가 원하는 타입을 입력하면 그걸로 변환해주는 범용성 좋은 
+
+type TypeChanger3 <MyType, T> = {
+  [key in keyof MyType]: T;
+};
+
+type NewBus = TypeChanger3<Bus, boolean>;
+type NewBus2 = TypeChanger3<Bus, string[]>
